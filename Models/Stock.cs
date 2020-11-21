99883f30dd714850d56eb0702617f3c1b0af8ae0
Kameron Bains <kameronsbains@gmail.com>
2020-11-21 23:24:58 +0000

    Basic Stick Class setup

---
+++ Models/Stock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Irish_Woollens_co_operative_e_commerce.Models
+{
+    public class Stock
+    {
+
+        //fields
+        private int product_Id
+        {
+            get { return product_Id; }
+            set { product_Id = value; }
+        }
+
+        private String name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private String description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int price
+        {
+            get { return price;  }
+            set { price = value; }
+        }
+
+        //constructor
+        public Stock(int id, String Name, String Description, int Price)
+        {
+            product_Id = id;
+            name = Name;
+            description = Description;
+            price = Price;
+        }
+
+        //methods
+
+    }
+}
